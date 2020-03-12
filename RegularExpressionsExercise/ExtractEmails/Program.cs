54c04bebe8b60ec4d4899ce5050c2083c2e9d0bb
kkaraivanov <55097619+kkaraivanov@users.noreply.github.com>
2020-03-12 18:11:12 +0200

    Extract Emails

---
+++ RegularExpressionsExercise/ExtractEmails/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ExtractEmails
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string pattern = @"((?<=\s)[a-zA-Z0-9]+([-.]\w*)*@[a-zA-Z]+?([.-][a-zA-Z]*)*(\.[a-z]{2,}))";
+            var matches = Regex.Matches(text, pattern);
+            var emails = new List<string>();
+
+            foreach (var match in matches)
+            {
+                emails.Add(match.ToString());
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, emails));
+        }
+    }
+}
