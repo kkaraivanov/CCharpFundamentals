e1eab909c7da0d33d9c0d11c7894d8764bb9eef9
Kostadin Karaivanov <kostadin.karaivanov@outlook.com>
2020-03-13 16:25:58 +0200

    Correct

--- RegularExpressionsMoreExercise/WinningTicket/Program.cs
+++ RegularExpressionsMoreExercise/WinningTicket/Program.cs
@@ -1,6 +1,7 @@
 ﻿namespace WinningTicket
 {
     using System;
+    using System.Collections.Generic;
     using System.Linq;
     using System.Text.RegularExpressions;
 
@@ -8,31 +9,44 @@
     {
         static void Main(string[] args)
         {
-            var tikets = Console.ReadLine()
-                .Split(new char[] { ' ', ','}, StringSplitOptions.RemoveEmptyEntries)
-                .ToArray();
+            var input = Console.ReadLine()
+                .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries);
+            var colectionTikets = new Dictionary<string, string>();
+            string simpleWin = @"[\@]{6,9}|[\#]{6,9}|[\$]{6,9}|[\^]{6,9}";
+            string jacpot = @"[\@]{10}|[\#]{10}|[\$]{10}|[\^]{10}";
 
-            foreach (var tiket in tikets)
+            foreach (var tiket in input)
             {
-                if(tiket.Length < 20)
+                if (tiket.Length != 20)
                 {
+                    colectionTikets.Add(tiket, "invalid ticket");
+                    continue;
+                }
+
+                string partLeft = tiket.Substring(0, 10);
+                string partRight = tiket.Substring(10, 10);
+                Match simpleWinLeft = Regex.Match(partLeft, simpleWin);
+                Match simpleWinRight = Regex.Match(partRight, simpleWin);
+                Match jacpotLeft = Regex.Match(partLeft, jacpot);
+                Match jacpotRight = Regex.Match(partLeft, jacpot);
+
+                if (jacpotLeft.Success && jacpotRight.Success)
+                    colectionTikets.Add(tiket, $"10{jacpotLeft.ToString()[0]} Jackpot!");
+                else if (simpleWinLeft.Success && simpleWinRight.Success && simpleWinLeft.ToString()[0].Equals(simpleWinRight.ToString()[0]))
+                {
+                    var count = Math.Min(simpleWinLeft.Length, simpleWinRight.Length);
+                    colectionTikets.Add(tiket, $"{count}{simpleWinLeft.ToString()[0]}");
+                }
+                else
+                    colectionTikets.Add(tiket, "no match");
+            }
+
+            foreach (KeyValuePair<string,string> tiket in colectionTikets)
+            {
+                if (tiket.Value.Equals("invalid ticket"))
                     Console.WriteLine("invalid ticket");
-                    continue;
-                }
-                
-                string pattern = @"^(?:[\w]+)?([\@\#\$\^]{10}|[\@\#\$\^]{6})(?:[\w]+)?(\1)";
-                var match = Regex.Match(tiket, pattern);
-
-                if (!match.Success) 
-                {
-                    Console.WriteLine($"ticket \"{tiket}\" - no match");
-                    continue;
-                }
-
-                if ((match.Groups[1].Value == match.Groups[2].Value) && match.Groups[1].Value.Length == 10)
-                    Console.WriteLine($"ticket \"{tiket}\" - 10{match.Groups[1].Value[0]} Jackpot!");
-                else if ((match.Groups[1].Value == match.Groups[2].Value) && match.Groups[1].Value.Length == 6)
-                    Console.WriteLine($"ticket \"{tiket}\" - 6{match.Groups[1].Value[0]}");
+                else
+                    Console.WriteLine($"ticket \"{tiket.Key}\" - {tiket.Value}");
             }
         }
     }
