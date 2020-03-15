936452566956b2d7d7726d795a4a8a27cc710abd
Kostadin Karaivanov <kostadin.karaivanov@outlook.com>
2020-03-15 17:41:14 +0200

    Post Office

---
+++ RegularExpressionsMoreExercise/PostOffice/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace PostOffice
+{
+    using System;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            var texts = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            var ferstPart = texts[0];
+            var ferstLetts = Regex.Match(ferstPart, @"(([#$%&*])([A-Z]+)(\2))").Groups[3].Value;
+            var secondPart = texts[1];
+            var threePart = texts[2];
+
+            foreach (var ferstLett in ferstLetts)
+            {
+                int asciiForFerstLett = ferstLett;
+                int wordLength = int.Parse(Regex.Match(secondPart, $@"{asciiForFerstLett}:([0-9][0-9])").Groups[1].Value);
+                string wordPart = $@"(?<=\s|^){ferstLett}[^\s]{{{wordLength}}}(?=\s|$)";
+                string word = Regex.Match(threePart, wordPart).ToString();
+
+                Console.WriteLine(word);
+            }
+
+        }
+    }
+}
