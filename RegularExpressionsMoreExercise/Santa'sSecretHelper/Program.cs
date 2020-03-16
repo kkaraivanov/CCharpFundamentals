92adcae9bf9c5d40300b07504507b1161269d958
Kostadin Karaivanov <kostadin.karaivanov@outlook.com>
2020-03-16 11:35:49 +0200

    Santa's Secret Helper

---
+++ RegularExpressionsMoreExercise/Santa'sSecretHelper/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Santa_sSecretHelper
+{
+    using System;
+    using System.Text;
+    using System.Text.RegularExpressions;
+
+    class Program
+    {
+        static void Main()
+        {
+            int.TryParse(Console.ReadLine(), out int key);
+            string commands = null;
+
+            while ((commands = Console.ReadLine()) != "end")
+            {
+                var sb = new StringBuilder();
+
+                for (int i = 0; i < commands.Length; i++)
+                {
+                    int temp = commands[i] - key;
+                    sb.Append(Convert.ToString((char)temp));
+                }
+                
+                var match = Regex.Match(sb.ToString(), @"[\@]([A-Za-z]+)[^@\-\!\:\>]*\!([GN])\!");
+                string name = match.Groups[1].ToString();
+                string behaviour = match.Groups[2].ToString();
+
+                if(behaviour == "G")
+                    Console.WriteLine(name);
+            }
+        }
+    }
+}
